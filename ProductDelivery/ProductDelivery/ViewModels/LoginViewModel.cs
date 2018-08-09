1c443381770003fcf3a8fee9540a97f2b7188306
Umesh Kamble <umesh.kamble@globallogic.com>
2018-08-09 18:16:14 +0530

    modify ui changes

--- ProductDelivery/ProductDelivery/ViewModels/LoginViewModel.cs
+++ ProductDelivery/ProductDelivery/ViewModels/LoginViewModel.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace ProductDelivery.ViewModels
 {
@@ -32,6 +34,11 @@ namespace ProductDelivery.ViewModels
             }
             else
            await App.Current.MainPage.DisplayAlert("Login failed", "Please try again!", "OK");
+        }
+        public ICommand GoToSignUpCommand => new Command(GoToSignUp);
+        private async void GoToSignUp()
+        {
+            await _navigationService.NavigateToViewModelAsync(typeof(SignUpViewModel));
         }
     }
 }
