32f36364ddb2d3028032a755161c40ceb69144f6
Umesh Kamble <umesh.kamble@globallogic.com>
2018-08-09 17:56:25 +0530

    Event effect added

---
+++ ProductDelivery/ProductDelivery/Effects/UnderlineTextEffect.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace ProductDelivery.Effects
+{
+    public class UnderlineTextEffect : RoutingEffect
+    {
+        public UnderlineTextEffect() : base("ProductDelivery.UnderlineTextEffect")
+        {
+        }
+    }
+}
