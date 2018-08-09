cb119253643c67954e50e02aed76c4e0dcca3d0f
Umesh Kamble <umesh.kamble@globallogic.com>
2018-08-09 18:23:07 +0530

    added page and viewmodel for signup page

---
+++ ProductDelivery/ProductDelivery/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Caliburn.Micro.Xamarin.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace ProductDelivery.ViewModels
+{
+    public class SignUpViewModel : BaseScreen
+    {
+        private readonly INavigationService _navigationService;
+        public SignUpViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string RepeatPassword { get; set; }
+
+        public async void RegisterCommand()
+        {
+            if (Password.Equals(RepeatPassword) && !string.IsNullOrEmpty(Username))
+            {
+                Preferences.Set("IsLogged", true);
+                await _navigationService.NavigateToViewModelAsync(typeof(MainViewModel));
+            }
+            else
+            {
+                await App.Current.MainPage.DisplayAlert("Password Not Match", "Please try again!", "OK");
+            }
+        }
+    }
+}
